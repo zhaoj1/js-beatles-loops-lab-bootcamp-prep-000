6e6ce444e7d4ec6ebab3f84281ce59774f0ee2ab
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-06-14 20:09:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ function theBeatlesPlay(musicians,instruments){
 function johnLennonFacts(facts){
   let i = 0
   while(i < facts.length){
-    
+    facts[i] = facts[i]+"!!!"
   }
+  return facts
 }
+
+function iLoveTheBeatles(n)
